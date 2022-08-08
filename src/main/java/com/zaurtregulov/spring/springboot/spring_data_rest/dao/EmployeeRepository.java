64271f938476d3a3c01672b7051b2065d2250ed3
Виктория Дробышевская <viktoria-petrovskaya@yandex.ru>
2022-08-08 18:47:43 +0300

    added new project spring data rest

---
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.zaurtregulov.spring.springboot.spring_data_rest.dao;
+
+import com.zaurtregulov.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
